f7b0c8705fb478525a4c03f3384d9a5305cb4a76
Peter <peterleite.dev@gmail.com>
2021-06-28 13:41:08 -0300

    exercicios de fixação

---
+++ Primeiro Bimestre/Programação Imperativa/16 junho/exercicio-Função-Contrutora.js
@@ -0,0 +1,11 @@
+// Meu cardápio
+
+// Crie uma função construtora chamada Restaurante, que receba como parâmetros o nome do restaurante e o cardápio (que será um array de strings).
+// Após isso chame a função entrada() dentro do objeto, que irá trazer uma mensagem de boas-vindas, informando o nome do restaurante, e o cardápio.
+
+const Restaurante = (nomeRestaurante, cardápio) => {
+    this.nomeRestaurante;
+    this.cardápio;
+}
+
+let restaurante1 = new ('Oba-Oba', 'salgados');
