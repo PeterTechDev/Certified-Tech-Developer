26d1d6c91353474cc70142f0118ecfff9f94947a
Peter Leite <peterleite.dev@gmail.com>
2021-07-19 20:15:55 -0300

    exercicios de condicionais

---
+++ Primeiro Bimestre/Programação Imperativa/Aula 04 - Controle de fluxo/exercicio-meu-primeiro-if.js
@@ -0,0 +1,13 @@
+// Com este exercício, vamos construir um if simples.
+
+// Declare uma variável com o nome status. Atribua a ela um valor booleano: (true ou false).
+
+// Em seguida, faça um if que avalie o valor contido nesta variável. Se o valor armazenado for true, imprima a seguinte mensagem no console: O valor é true - verdadeiro. Caso contrário, imprima a mensagem: O valor é false - falso.
+
+const status = false;
+
+if (status === true) {
+    console.log("O valor é true - verdadeiro")
+} else if (status === false) {
+    console.log("O valor é false - falso")
+}
---
+++ Primeiro Bimestre/Programação Imperativa/Aula 04 - Controle de fluxo/readme.md
@@ -0,0 +1,17 @@
+<h1>Exercício: Meu primeiro if</h1>
+
+<h2>Objetivo:</h2>
+<p>Entender como é a sintaxe de uma estrutura condicional.</p>
+<br>
+<br>
+<p>
+    Com este exercício, vamos construir um if simples.
+    <br>
+
+    Declare uma variável com o nome status. Atribua a ela um valor booleano: (true ou false).
+    <br>
+
+    Em seguida, faça um if que avalie o valor contido nesta variável. Se o valor armazenado for true, imprima a seguinte
+    mensagem no console: O valor é true - verdadeiro. Caso contrário, imprima a mensagem: O valor é false - falso.
+
+</p>
