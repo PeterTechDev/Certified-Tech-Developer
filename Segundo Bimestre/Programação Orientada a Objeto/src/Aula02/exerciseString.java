9d9e004dacfb87e1dce5d151fb024e183c2b7da5
Peter Leite <peterleite.dev@gmail.com>
2021-08-13 17:05:23 -0300

    exercicio comparar strings em JAVA

---
+++ Segundo Bimestre/Programação Orientada a Objeto/src/Aula02/exerciseString.java
@@ -0,0 +1,16 @@
+package Aula02;
+
+public class exerciseString {
+    public static void main (String[] args){
+
+        String quote1 = "Be yourself; everyone else is already taken";
+        String quote2 = "You only live once, but if you do it right, once is enough";
+
+        if(quote1.equals(quote2)){
+            System.out.println("As frases são iguais");
+        } else {
+            System.out.println("As frases são diferentes");
+        }
+
+    }
+}
