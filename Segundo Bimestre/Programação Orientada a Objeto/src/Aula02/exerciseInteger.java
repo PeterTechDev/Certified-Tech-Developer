448c413ed85a2bcbc9812909173811abc78e9c02
Peter Leite <peterleite.dev@gmail.com>
2021-08-13 17:21:21 -0300

    exercicio comparar Integer(numeros inteiros)

---
+++ Segundo Bimestre/Programação Orientada a Objeto/src/Aula02/exerciseInteger.java
@@ -0,0 +1,16 @@
+package Aula02;
+
+//import.lang.Integer;
+
+public class exerciseInteger {
+    public static void main(String[] args) {
+        Integer first = 3;
+        Integer second = 16;
+
+        if(first < second) {
+            System.out.println("O primeiro é menor");
+        } else {
+            System.out.println("O segundo é maior");
+        }
+    }
+}
