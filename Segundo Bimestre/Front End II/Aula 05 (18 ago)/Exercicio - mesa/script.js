c076067d1d602cc95e6cfa1a0d17f68e2018b8cc
Peter Leite <peterleite.dev@gmail.com>
2021-08-18 10:31:48 -0300

    inicio exercio frontend - botao darkmode

---
+++ Segundo Bimestre/Front End II/Aula 05 (18 ago)/Exercicio - mesa/script.js
@@ -0,0 +1,14 @@
+button = document.querySelector("button");
+darkModeBtn = document.getElementById('btn-darkmode');
+body = document.querySelector('body');
+title = document.querySelector('h1');
+
+
+const nightMode =()=> {
+
+}
+
+//mudar a cor do BG  do body
+body.style.backgroundColor = "#000000";
+title.style.color = "red"
+
