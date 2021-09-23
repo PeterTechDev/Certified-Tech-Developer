40485560fdd7517094c3a0419361351ed68a4b57
Peter Leite <peterleite.dev@gmail.com>
2021-09-23 16:07:52 -0300

    exercicio usando trim, toUpperCase, toLowerCase, concat e replace

--- Segundo Bimestre/Front End II/Aula 11 (01 set)  - Formlulario de novo/Atividade/script.js
+++ Segundo Bimestre/Front End II/Aula 11 (01 set)  - Formlulario de novo/Atividade/script.js
@@ -1,16 +1,23 @@
 // const nomeInput = document.getElementById('nome')
 // const sobrenomeInput = document.getElementById('sobrenome')
 let container = document.querySelector(".container");
+document.getElementById('trim')
+let nomeInput = document.getElementById('nome')
+let sobrenomeInput = document.getElementById('sobrenome')
+let fullName = '';
 
 const submit = () => {
-    let nomeInput = document.getElementById('nome').value
-    let sobrenomeInput = document.getElementById('sobrenome').value
-    // trim(), toUpperCase(), toLowerCase().
-    let element = "";
-    for
-    let fullName = nomeInput + '' + sobrenomeInput
-        element += `<p>${container.innerText = fullName.trim()}</p>`;
-        console.log('aqui')
+    fullName = nomeInput.value + '' + sobrenomeInput.value
+    trim(fullName)
+    upCase(fullName)
+    lwCase(fullName)
+    concat(fullName)
+    replace(fullName)
 }
 
-// console.log(container)
+
+const trim =()=> document.getElementById('trim').innerHTML = fullName.trim()
+const upCase =()=> document.getElementById('toUpperCase').innerHTML = fullName.toLocaleUpperCase()
+const lwCase =()=> document.getElementById('toLowerCase').innerHTML = fullName.toLocaleLowerCase()
+const concat =()=> document.getElementById('concat').innerHTML = nomeInput.value.concat(sobrenomeInput.value)
+const replace =()=> document.getElementById('replace').innerHTML = (fullName.replace('a','@')).replace('e', '3')
