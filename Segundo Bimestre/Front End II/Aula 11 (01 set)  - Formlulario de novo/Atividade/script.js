c208fdc023a61a83c967b48385c237488448ab1c
Peter Leite <peterleite.dev@gmail.com>
2021-09-23 15:03:07 -0300

    iniciando exercicios de frotend

---
+++ Segundo Bimestre/Front End II/Aula 11 (01 set)  - Formlulario de novo/Atividade/script.js
@@ -0,0 +1,16 @@
+// const nomeInput = document.getElementById('nome')
+// const sobrenomeInput = document.getElementById('sobrenome')
+let container = document.querySelector(".container");
+
+const submit = () => {
+    let nomeInput = document.getElementById('nome').value
+    let sobrenomeInput = document.getElementById('sobrenome').value
+    // trim(), toUpperCase(), toLowerCase().
+    let element = "";
+    for
+    let fullName = nomeInput + '' + sobrenomeInput
+        element += `<p>${container.innerText = fullName.trim()}</p>`;
+        console.log('aqui')
+}
+
+// console.log(container)
