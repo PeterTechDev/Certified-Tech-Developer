969fb3918da30c9af960379fa489c1fcf6ea82f0
Peter Leite <peterleite.dev@gmail.com>
2021-09-24 02:39:07 -0300

    added função para verificar se o comentário já existe

--- Segundo Bimestre/Front End II/Aula 14 - JSON e Storage/Atividade/script.js
+++ Segundo Bimestre/Front End II/Aula 14 - JSON e Storage/Atividade/script.js
@@ -1,16 +1,58 @@
 const board = document.querySelector('.board')
 const nameInput = document.querySelector('#name')
 const themeInput = document.querySelector('#theme')
-const textareaInput = document.querySelector('#textarea')
+const comentaryInput = document.querySelector('#textarea')
 
+// let dataBase = JSON.parse(localStorage.getItem(dataBase)) || {}
+let questions = JSON.parse(localStorage.getItem('DBquestions')) || []
 
-document.querySelector('button').onclick =()=> writeBoard()
-
-function writeBoard(){
-    board
-    .innerHTML += `<h3>${nameInput.value}</h3>`
-    board
-    .innerHTML += `<p>${themeInput.value}</p>`
-    board
-    .innerHTML += `<p>${textareaInput.value}</p>`
+function Question(name, theme, comentary) {
+    this.name = name;
+    this.theme = theme;
+    this.comentary = comentary;
 }
+
+// let question2 = new Question ('Peter', 'CSS', 'Não entendo positoin até hoje')
+
+function createObjQuestion(name, theme, comentary) {
+    questions.push(new Question(name.value, theme.value, comentary.value))
+}
+
+function checkIfExist (event){
+    for (let index = 0; index < questions.length; index++) {
+        if(questions[index].comentary == comentaryInput.value){
+            alert('Já existe')
+            event.prevantDefault()
+        }
+    }
+}
+
+function setStorage() {
+    localStorage.setItem('DBquestions', JSON.stringify(questions))
+}
+
+// function getState (){
+//     const obj = 
+// }
+
+const clearLocalStorage = () => localStorage.clear()
+
+document.querySelector('button').onclick = () => {
+    checkIfExist()
+    createObjQuestion(nameInput, themeInput, comentaryInput)
+    setStorage()
+    writeBoard()
+}
+
+function writeBoard() {
+    for (index = 0; index < questions.length; index++) {
+        board
+            .innerHTML += `<h3>${questions[index].name}</h3>`
+        board
+            .innerHTML += `<p>${questions[index].theme}</p>`
+        board
+            .innerHTML += `<p>${questions[index].comentary}</p>`
+    }
+}
+writeBoard()
+
