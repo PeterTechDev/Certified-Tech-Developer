f7e7b5251d06975763e949494926a20ff28f5952
Peter Leite <peterleite.dev@gmail.com>
2021-09-26 11:52:04 -0300

    revisando/refazendo POO

---
+++ Segundo Bimestre/POO/src/Aula02Variaveis/exercicioSimplesVariaveis.java
@@ -0,0 +1,15 @@
+package Aula02Variaveis;
+
+//Defina dois números inteiros. Atribua um valor a cada um.
+//Verifique se um número é divisível pelo segundo e indique o resultado obtido por meio de uma mensagem.
+
+//NOTA: O fato de ser divisível significa que ao dividir precisa dar um valor exato, ou seja, sem decimais.
+
+public class exercicioSimplesVariaveis {
+    public static void main(String[] args) {
+        int numA = 10;
+        int numB = 2;
+
+        System.out.println("O resultado de numA divido por NumB: " + numA/numB);
+    }
+}
